ad1188a841d25ffafe7a6376ae7a86fb6a576a23
Mark Crowder <crowdered@gmail.com>
2019-03-19 14:21:36 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,4 @@
-def hello_t(["Tim", "Tom", "Jim"]) do |name| 
-  if name.start_with? ("T")
-    puts "Hi, #{name}"
-  end
-end
+def hello_t(array)
 i = 0
 
 while i < array.length
@@ -11,4 +7,8 @@ i = i + 1
 end
 end
 
-
+hello_t(["Tim", "Tom", "Jim"]) do |name| 
+  if name.start_with? ("T")
+    puts "Hi, #{name}"
+  end
+end
